f6cc4f9ab785e2767dcc052d807d17832734c9d3
Neven <neven@orgzly.com>
2017-02-01 02:05:48 -0500

    Handle deprecated fromHtml

--- app/src/main/java/com/orgzly/android/ui/dialogs/WhatsNewDialog.java
+++ app/src/main/java/com/orgzly/android/ui/dialogs/WhatsNewDialog.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.text.Html;
+import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
@@ -21,11 +22,11 @@ public class WhatsNewDialog {
         TextView view;
 
         view = ((TextView) layoutView.findViewById(R.id.dialog_whats_new_info));
-        view.setText(Html.fromHtml(activity.getString(R.string.whats_new_info)));
+        view.setText(fromHtml(activity.getString(R.string.whats_new_info)));
         view.setMovementMethod(LinkMovementMethod.getInstance());
 
         view = ((TextView) layoutView.findViewById(R.id.dialog_whats_new_follow));
-        view.setText(Html.fromHtml(activity.getString(R.string.whats_new_follow)));
+        view.setText(fromHtml(activity.getString(R.string.whats_new_follow)));
         view.setMovementMethod(LinkMovementMethod.getInstance());
 
         return new AlertDialog.Builder(activity)
@@ -33,5 +34,16 @@ public class WhatsNewDialog {
                 .setPositiveButton(R.string.ok, null)
                 .setView(layoutView)
                 .create();
+    }
+
+    @SuppressWarnings("deprecation")
+    private static Spanned fromHtml(String html){
+        Spanned result;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            result = Html.fromHtml(html,Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            result = Html.fromHtml(html);
+        }
+        return result;
     }
 }
