a26fce1962b8d6c40bcb522f440f2f6fc8bf4e9c
Neven <neven@orgzly.com>
2017-04-09 14:08:02 +0900

    Display inherited tags in search for tests

--- app/src/main/java/com/orgzly/android/prefs/AppPreferences.java
+++ app/src/main/java/com/orgzly/android/prefs/AppPreferences.java
@@ -214,6 +214,11 @@ public class AppPreferences {
                 context.getResources().getBoolean(R.bool.pref_default_display_inherited_tags_in_search_results));
     }
 
+    public static void inheritedTagsInSearchResults(Context context, boolean value) {
+        String key = context.getResources().getString(R.string.pref_key_display_inherited_tags_in_search_results);
+        getDefaultSharedPreferences(context).edit().putBoolean(key, value).apply();
+    }
+
     public static String fontSize(Context context) {
         return getDefaultSharedPreferences(context).getString(
                 context.getResources().getString(R.string.pref_key_font_size),
