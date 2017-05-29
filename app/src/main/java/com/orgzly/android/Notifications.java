af042271028dc5d2fd26409de7674e8d57203b47
Mackie Loeffel <mackie.loeffel@web.de>
2017-05-29 21:03:46 +0200

    Add sync action to new note notification

--- app/src/main/java/com/orgzly/android/Notifications.java
+++ app/src/main/java/com/orgzly/android/Notifications.java
@@ -4,10 +4,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import com.orgzly.BuildConfig;
 import com.orgzly.R;
+import com.orgzly.android.sync.SyncService;
 import com.orgzly.android.ui.ShareActivity;
 import com.orgzly.android.util.LogUtils;
 
@@ -23,21 +25,29 @@ public class Notifications {
         PendingIntent resultPendingIntent = ShareActivity.createNewNoteIntent(context);
 
         /* Build notification */
-        Notification notification =
-                new NotificationCompat.Builder(context)
-                        .setOngoing(true)
-                        .setSmallIcon(R.drawable.cic_orgzly_notification)
-                        .setContentTitle(context.getString(R.string.new_note))
-                        .setContentText(context.getString(R.string.tap_to_create_new_note))
-                        .setColor(ContextCompat.getColor(context, R.color.notification))
-                        .setContentIntent(resultPendingIntent)
-                        .setPriority(NotificationCompat.PRIORITY_MIN) // Don't show icon on status bar
-                        .build();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.cic_orgzly_notification)
+                .setContentTitle(context.getString(R.string.new_note))
+                .setContentText(context.getString(R.string.tap_to_create_new_note))
+                .setColor(ContextCompat.getColor(context, R.color.notification))
+                .setContentIntent(resultPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_MIN); // Don't show icon on status bar
+
+        /* add sync action */
+        Intent syncIntent = new Intent(context, SyncService.class);
+        syncIntent.setAction(AppIntent.ACTION_SYNC_START);
+
+        PendingIntent syncPendingIntent = PendingIntent.getService(context, 0, syncIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.addAction(
+                    R.drawable.ic_sync_white_24dp,
+                    context.getString(R.string.sync),
+                    syncPendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(ONGOING_NEW_NOTE, notification);
+        notificationManager.notify(ONGOING_NEW_NOTE, builder.build());
     }
 
     public static void cancelNewNoteNotification(Context context) {
