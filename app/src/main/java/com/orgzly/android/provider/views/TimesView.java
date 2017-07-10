a3337b090fe1d831d635323b3f57fe28cfe588cf
Neven <neven@orgzly.com>
2017-07-10 14:50:47 +0200

    Use constants in TimesView

--- app/src/main/java/com/orgzly/android/provider/views/TimesView.java
+++ app/src/main/java/com/orgzly/android/provider/views/TimesView.java
@@ -1,5 +1,10 @@
 package com.orgzly.android.provider.views;
 
+import com.orgzly.android.provider.models.DbBook;
+import com.orgzly.android.provider.models.DbNote;
+import com.orgzly.android.provider.models.DbOrgRange;
+import com.orgzly.android.provider.models.DbOrgTimestamp;
+
 public class TimesView {
     static public final String VIEW_NAME = "times_view";
 
@@ -8,37 +13,32 @@ public class TimesView {
     static public final String CREATE_SQL =
             "CREATE VIEW " + VIEW_NAME + " AS " +
             "  SELECT\n" +
-            "  n._id as note_id,\n" +
-            "  n.book_id as book_id,\n" +
-            "  coalesce(b.title, b.name) as book_name,\n" +
-            "  n.state as note_state,\n" +
-            "  substr(n.title, 0, 30) as note_title,\n" +
-            "  1 as time_type,\n" +
-            "  t.timestamp as timestamp,\n" +
-            "  t.string as org_timestamp_string,\n" +
-            "  (t.hour IS NOT NULL AND t.hour != \"\") as has_time_part\n" +
-            "  FROM org_ranges r\n" +
-            "  JOIN org_timestamps t ON (r.start_timestamp_id = t._id)\n" +
-            "  JOIN notes n ON (r._id = n.scheduled_range_id)\n" +
-            "  JOIN books b ON (b._id = n.book_id)\n" +
-            "  WHERE t.is_active = 1\n" +
+            "  n." + DbNote.Column._ID + " as " + Columns.NOTE_ID + ",\n" +
+            "  n." + DbNote.Column.BOOK_ID + " as " + Columns.BOOK_ID + ",\n" +
+            "  coalesce(b." + DbBook.Column.TITLE + ", b." + DbBook.Column.NAME + ") as " + Columns.BOOK_NAME + ",\n" +
+            "  n." + DbNote.Column.STATE + " as " + Columns.NOTE_STATE + ",\n" +
+            "  n." + DbNote.Column.TITLE + " as " + Columns.NOTE_TITLE + ",\n" +
+            "  1 as " + Columns.TIME_TYPE + ",\n" +
+            "  t." + DbOrgTimestamp.Column.STRING + " as " + Columns.ORG_TIMESTAMP_STRING + "\n" +
+            "  FROM " + DbOrgRange.TABLE + " r\n" +
+            "  JOIN " + DbOrgTimestamp.TABLE + " t ON (r." + DbOrgRange.Column.START_TIMESTAMP_ID + " = t." + DbOrgTimestamp.Column._ID + " )\n" +
+            "  JOIN " + DbNote.TABLE + " n ON (r." + DbOrgRange.Column._ID + " = n." + DbNote.Column.SCHEDULED_RANGE_ID + ")\n" +
+            "  JOIN " + DbBook.TABLE + " b ON (b." + DbBook.Column._ID + " = n." + DbNote.Column.BOOK_ID + ")\n" +
+            "  WHERE t." + DbOrgTimestamp.Columns.IS_ACTIVE + " = 1\n" +
             "UNION\n" +
             "  SELECT\n" +
-            "  n._id as note_id,\n" +
-            "  n.book_id as book_id,\n" +
-            "  coalesce(b.title, b.name) as book_name,\n" +
-            "  n.state as note_state,\n" +
-            "  substr(n.title, 0, 30) as note_title,\n" +
-            "  2 as time_type,\n" +
-            "  t.timestamp as timestamp,\n" +
-            "  t.string as org_timestamp_string,\n" +
-            "  (t.hour IS NOT NULL AND t.hour != \"\") as has_time_part\n" +
-            "  FROM org_ranges r\n" +
-            "  JOIN org_timestamps t ON (r.start_timestamp_id = t._id)\n" +
-            "  JOIN notes n ON (r._id = n.deadline_range_id)\n" +
-            "  JOIN books b ON (b._id = n.book_id)\n" +
-            "  WHERE t.is_active = 1\n" +
-            "ORDER BY note_id";
+            "  n." + DbNote.Column._ID + " as " + Columns.NOTE_ID + ",\n" +
+            "  n." + DbNote.Column.BOOK_ID + " as " + Columns.BOOK_ID + ",\n" +
+            "  coalesce(b." + DbBook.Column.TITLE + ", b." + DbBook.Column.NAME + ") as " + Columns.BOOK_NAME + ",\n" +
+            "  n." + DbNote.Column.STATE + " as " + Columns.NOTE_STATE + ",\n" +
+            "  n." + DbNote.Column.TITLE + " as " + Columns.NOTE_TITLE + ",\n" +
+            "  1 as " + Columns.TIME_TYPE + ",\n" +
+            "  t." + DbOrgTimestamp.Column.STRING + " as " + Columns.ORG_TIMESTAMP_STRING + "\n" +
+            "  FROM " + DbOrgRange.TABLE + " r\n" +
+            "  JOIN " + DbOrgTimestamp.TABLE + " t ON (r." + DbOrgRange.Column.START_TIMESTAMP_ID + " = t." + DbOrgTimestamp.Column._ID + " )\n" +
+            "  JOIN " + DbNote.TABLE + " n ON (r." + DbOrgRange.Column._ID + " = n." + DbNote.Column.DEADLINE_RANGE_ID + ")\n" +
+            "  JOIN " + DbBook.TABLE + " b ON (b." + DbBook.Column._ID + " = n." + DbNote.Column.BOOK_ID + ")\n" +
+            "  WHERE t." + DbOrgTimestamp.Columns.IS_ACTIVE + " = 1\n";
 
     public interface Columns {
         String NOTE_ID = "note_id";
