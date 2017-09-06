081ab46192291cfb2e691c1b95d3d80d5a8026d1
Neven <neven@orgzly.com>
2017-09-06 22:50:12 +0200

    Support emphasis and monospace (resolves #20)

--- app/src/main/java/com/orgzly/android/util/OrgFormatter.java
+++ app/src/main/java/com/orgzly/android/util/OrgFormatter.java
@@ -1,8 +1,13 @@
 package com.orgzly.android.util;
 
+import android.graphics.Typeface;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.TypefaceSpan;
 import android.text.style.URLSpan;
+import android.text.style.UnderlineSpan;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,6 +39,7 @@ public class OrgFormatter {
         doOrgLinksWithName(ssb, BRACKET_ANY_LINK, false);
         doOrgLinks(ssb, BRACKET_LINK, linkify);
         doPlainLinks(ssb, PLAIN_LINK, linkify);
+        doMarkup(ssb);
 
         return ssb;
     }
@@ -55,7 +61,7 @@ public class OrgFormatter {
                 setUrlSpan(ssb, link, m.start(), m.start() + name.length());
             }
 
-            /* Must re-create Matcher, as ssb size is modified. */
+            /* Re-create Matcher, as ssb size is modified. */
             m = p.matcher(ssb);
 
         }
@@ -72,12 +78,12 @@ public class OrgFormatter {
             String link = m.group(1);
 
             ssb.replace(m.start(), m.end(), link);
-            
+
             if (createLinks) {
                 setUrlSpan(ssb, link, m.start(), m.start() + link.length());
             }
 
-            /* Must re-create Matcher, as ssb size is modified. */
+            /* Re-create Matcher, as ssb size is modified. */
             m = p.matcher(ssb);
         }
     }
@@ -89,7 +95,7 @@ public class OrgFormatter {
         if (!createLinks) {
             return;
         }
-        
+
         Pattern p = Pattern.compile(linkRegex);
         Matcher m = p.matcher(ssb);
 
@@ -104,6 +110,53 @@ public class OrgFormatter {
     }
 
     private static void setUrlSpan(SpannableStringBuilder ssb, String link, int start, int end) {
-        ssb.setSpan(new URLSpan(link), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(new URLSpan(link), start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+    }
+
+    /**
+     * You can make words *bold*, /italic/, _underlined_, =verbatim= , ~code~ and +strike-through+.
+     */
+    private static void doMarkup(SpannableStringBuilder ssb) {
+        Pattern p = Pattern.compile("(\\*([^*\\s]+)\\*)|(/([^/\\s]+)/)|(_([^_\\s]+)_)|(=([^=\\s]+)=)|(~([^~\\s]+)~)|(\\+([^+\\s]+)\\+)");
+        Matcher m = p.matcher(ssb);
+
+        while (m.find()) {
+            String content = null;
+            Object span = null;
+
+            if (m.group(1) != null) {
+                content = m.group(2);
+                span = new StyleSpan(Typeface.BOLD);
+
+            } else if (m.group(3) != null) {
+                content = m.group(4);
+                span = new StyleSpan(Typeface.ITALIC);
+
+            } else if (m.group(5) != null) {
+                content = m.group(6);
+                span = new UnderlineSpan();
+
+            } else if (m.group(7) != null) {
+                content = m.group(8);
+                span = new TypefaceSpan("monospace");
+
+            } else if (m.group(9) != null) {
+                content = m.group(10);
+                span = new TypefaceSpan("monospace");
+
+            } else if (m.group(11) != null) {
+                content = m.group(12);
+                span = new StrikethroughSpan();
+            }
+
+            if (span != null && content != null) {
+                ssb.replace(m.start(), m.end(), content);
+
+                ssb.setSpan(span, m.start(), m.end()-2, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
+                /* Re-create Matcher, as ssb size is modified. */
+                m = p.matcher(ssb);
+            }
+        }
     }
 }
