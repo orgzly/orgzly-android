f5f109efb681550c49923c736dd0d01742518bd7
Neven <neven@orgzly.com>
2017-11-24 15:00:10 +0100

    Handle missing original ID in agenda

--- app/src/main/java/com/orgzly/android/ui/fragments/AgendaFragment.java
+++ app/src/main/java/com/orgzly/android/ui/fragments/AgendaFragment.java
@@ -47,7 +47,6 @@ import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
@@ -533,12 +532,10 @@ public class AgendaFragment extends NoteListFragment
             TreeSet<Long> selectionIds;
             switch (menuItem.getItemId()) {
                 case R.id.query_cab_schedule:
-                    selectionIds = new TreeSet<>();
-                    for (Long id: mSelection.getIds()) {
-                        Long originalId = originalNoteIDs.get(id);
-                        selectionIds.add(originalId);
+                    selectionIds = originalSelectedIds();
+                    if (!selectionIds.isEmpty()) {
+                        displayScheduleTimestampDialog(R.id.query_cab_schedule, selectionIds);
                     }
-                    displayScheduleTimestampDialog(R.id.query_cab_schedule, selectionIds);
                     break;
 
                 case R.id.query_cab_state:
@@ -556,12 +553,10 @@ public class AgendaFragment extends NoteListFragment
                     /* Click on one of the state keywords. */
                     if (menuItem.getGroupId() == STATE_ITEM_GROUP) {
                         if (mListener != null) {
-                            selectionIds = new TreeSet<>();
-                            for (Long id: mSelection.getIds()) {
-                                Long originalId = originalNoteIDs.get(id);
-                                selectionIds.add(originalId);
+                            selectionIds = originalSelectedIds();
+                            if (!selectionIds.isEmpty()) {
+                                mListener.onStateChangeRequest(selectionIds, menuItem.getTitle().toString());
                             }
-                            mListener.onStateChangeRequest(selectionIds, menuItem.getTitle().toString());
                         }
                         return true;
                     }
@@ -570,6 +565,22 @@ public class AgendaFragment extends NoteListFragment
             }
 
             return true; // Handled.
+        }
+
+        private TreeSet<Long> originalSelectedIds() {
+            TreeSet<Long> selectionIds = new TreeSet<>();
+            for (Long id: mSelection.getIds()) {
+                Long originalId = originalNoteIDs.get(id);
+                /*
+                 * Original ID might be missing if user selects a note before it's gone
+                 * (because of sync re-loading a notebook for example).  Adding null to TreeSet
+                 * throws NullPointerException.  TODO: De-select notes and remove this check
+                 */
+                if (originalId != null) {
+                    selectionIds.add(originalId);
+                }
+            }
+            return selectionIds;
         }
 
         @Override
