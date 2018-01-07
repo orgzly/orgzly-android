2da4590626f8c0cba3641f1bba437bc526e48a4c
Neven <neven@orgzly.com>
2018-01-07 20:23:43 +0100

    Add changes for v1.6.2-beta.1

--- app/src/main/java/com/orgzly/android/ui/views/WhatsNewChange.java
+++ app/src/main/java/com/orgzly/android/ui/views/WhatsNewChange.java
@@ -2,6 +2,7 @@ package com.orgzly.android.ui.views;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.method.LinkMovementMethod;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.orgzly.R;
+
+import static android.text.Html.fromHtml;
 
 public class WhatsNewChange extends LinearLayout {
     public WhatsNewChange(Context context, AttributeSet attrs) {
@@ -21,7 +24,9 @@ public class WhatsNewChange extends LinearLayout {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.text_list_item, this, true);
 
-        ((TextView) view.findViewById(R.id.content)).setText(content);
+        TextView c = ((TextView) view.findViewById(R.id.content));
+        c.setText(fromHtml(content));
+        c.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
 
