239ed9212aca1e850c2ca60b6376d1483cd6b63d
Neven <neven@orgzly.com>
2018-10-19 10:43:00 +0200

    Convert OrgFormatterLinkTest to parameterized test

--- app/src/androidTest/java/com/orgzly/android/util/OrgFormatterLinkTest.kt
+++ app/src/androidTest/java/com/orgzly/android/util/OrgFormatterLinkTest.kt
@@ -3,16 +3,26 @@ package com.orgzly.android.util
 import android.os.Environment
 import android.text.style.ClickableSpan
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.Matchers.instanceOf
+import org.hamcrest.Matchers.not
 import org.junit.Assert.assertThat
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import java.io.File
 
-@Ignore // TODO: WIP
-@Suppress("TestFunctionName")
-class OrgFormatterLinkTest : OrgFormatterTest() {
+@RunWith(value = Parameterized::class)
+class OrgFormatterLinkTest(private val param: Parameter) : OrgFormatterTest() {
+
+    data class Parameter(
+            val input: String,
+            val output: String,
+            val size: Int,
+            val start: Int,
+            val end: Int,
+            val clickable: Boolean)
+
     @Before
     @Throws(Exception::class)
     override fun setUp() {
@@ -29,46 +39,39 @@ class OrgFormatterLinkTest : OrgFormatterTest() {
         }
     }
 
-    @Test
-    fun SquareBrackets_File_Document_Existing() {
-        val spannable = OrgSpannable("[[file:orgzly-tests/document.txt]]")
 
-        assertThat(spannable.string, `is`("file:orgzly-tests/document.txt"))
-        assertThat(spannable.spans.size, `is`(1))
-        assertThat(spannable.spans[0].start, `is`(0))
-        assertThat(spannable.spans[0].end, `is`(17))
+    companion object {
+        @JvmStatic @Parameterized.Parameters(name = "{index}: {0}")
+        fun data(): Collection<Parameter> {
+            return listOf(
+                    Parameter("[[orgzly-tests/document.txt]]", "orgzly-tests/document.txt", 1, 0, 25, true),
+
+                    Parameter("file:orgzly-tests/document.txt", "file:orgzly-tests/document.txt", 1, 0, 30, true),
+                    Parameter("[[file:orgzly-tests/document.txt]]", "file:orgzly-tests/document.txt", 1, 0, 30, true),
+                    Parameter("[[file:orgzly-tests/document.txt][Document]]", "Document", 1, 0, 8, true),
+
+                    Parameter("id:45DFE015-255E-4B86-B957-F7FD77364DCA", "id:45DFE015-255E-4B86-B957-F7FD77364DCA", 1, 0, 39, true),
+                    Parameter("[[id:45DFE015-255E-4B86-B957-F7FD77364DCA]]", "id:45DFE015-255E-4B86-B957-F7FD77364DCA", 1, 0, 39, true),
+
+                    Parameter("mailto:a@b.com", "mailto:a@b.com", 1, 0, 14, true),
+                    Parameter("[[mailto:a@b.com]]", "mailto:a@b.com", 1, 0, 14, true)
+            )
+        }
     }
 
     @Test
-    fun NamedSquareBrackets_File_Document_Existing() {
-        val spannable = OrgSpannable("[[file:orgzly-tests/document.txt][Document]]")
+    fun testLink() {
+        val spannable = OrgSpannable(param.input)
 
-        assertThat(spannable.string, `is`("Document"))
-        assertThat(spannable.spans.size, `is`(1))
-        assertThat(spannable.spans[0].start, `is`(0))
-        assertThat(spannable.spans[0].end, `is`(8))
-        assertThat(spannable.spans[0].span, instanceOf(ClickableSpan::class.java))
-    }
+        assertThat(spannable.string, `is`(param.output))
+        assertThat(spannable.spans.size, `is`(param.size))
+        assertThat(spannable.spans[0].start, `is`(param.start))
+        assertThat(spannable.spans[0].end, `is`(param.end))
 
-    @Test
-    fun PlainLink_Id_UUID_Valid() {
-        val spannable = OrgSpannable("id:45DFE015-255E-4B86-B957-F7FD77364DCA")
-
-        assertThat(spannable.string, `is`("id:45DFE015-255E-4B86-B957-F7FD77364DCA"))
-        assertThat(spannable.spans.size, `is`(1))
-        assertThat(spannable.spans[0].start, `is`(0))
-        assertThat(spannable.spans[0].end, `is`(39))
-        assertThat(spannable.spans[0].span, instanceOf(ClickableSpan::class.java))
-    }
-
-    @Test
-    fun SquareBrackets_Id_UUID_Valid() {
-        val spannable = OrgSpannable("[[id:45DFE015-255E-4B86-B957-F7FD77364DCA]]")
-
-        assertThat(spannable.string, `is`("id:45DFE015-255E-4B86-B957-F7FD77364DCA"))
-        assertThat(spannable.spans.size, `is`(1))
-        assertThat(spannable.spans[0].start, `is`(0))
-        assertThat(spannable.spans[0].end, `is`(39))
-        assertThat(spannable.spans[0].span, instanceOf(ClickableSpan::class.java))
+        if (param.clickable) {
+            assertThat(spannable.spans[0].span, instanceOf(ClickableSpan::class.java))
+        } else {
+            assertThat(spannable.spans[0].span, not(instanceOf(ClickableSpan::class.java)))
+        }
     }
 }
