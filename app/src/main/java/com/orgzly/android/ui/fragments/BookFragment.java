eabcb141f9c66c0077abd86ee11e8e8b6f17e3d1
oujunhao <aaron@theolsens.ca>
2018-10-27 19:36:07 -0400

    Remove state variable for keeping screen on

--- app/src/main/java/com/orgzly/android/ui/fragments/BookFragment.java
+++ app/src/main/java/com/orgzly/android/ui/fragments/BookFragment.java
@@ -101,8 +101,6 @@ public class BookFragment extends NoteListFragment
 
     private String mActionModeTag;
 
-    private static boolean keepScreenOn = false;
-
     /** Used to switch to book-does-not-exist view, if the book has been deleted. */
     private ViewFlipper mViewFlipper;
 
@@ -419,25 +417,24 @@ public class BookFragment extends NoteListFragment
                 return true;
 
             case R.id.books_options_keep_screen_on:
-                if (!keepScreenOn) {
+                boolean keepScreenOnEnabled = (getActivity().getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0;
+                if (!keepScreenOnEnabled) {
                     String title = "Keep screen on";
                     String message = "Your screen will not turn off until you disable this option or leave this page";
                     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                     builder.setTitle(title);
                     builder.setMessage(message);
                     builder.setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                        keepScreenOn = true;
                         getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                        item.setChecked(keepScreenOn);
+                        item.setChecked(true);
                         dialog.dismiss();
                     });
                     builder.setNegativeButton(android.R.string.cancel, (dialog, id) -> dialog.dismiss());
                     AlertDialog dialog = builder.create();
                     dialog.show();
                 } else {
-                    keepScreenOn = false;
                     getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                    item.setChecked(keepScreenOn);
+                    item.setChecked(false);
                 }
                 return true;
 
