1fd8c8349976a32f4d8361b6782d4b44dd5d2f62
Neven <neven@orgzly.com>
2018-11-02 07:12:36 +0100

    Add test for relative-file notebook.

--- app/src/androidTest/java/com/orgzly/android/espresso/InternalLinksTest.kt
+++ app/src/androidTest/java/com/orgzly/android/espresso/InternalLinksTest.kt
@@ -1,21 +1,18 @@
 package com.orgzly.android.espresso
 
-import android.support.test.rule.ActivityTestRule
-
-import com.orgzly.R
-import com.orgzly.android.OrgzlyTest
-import com.orgzly.android.ui.MainActivity
-
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import com.orgzly.R
+import com.orgzly.android.OrgzlyTest
 import com.orgzly.android.espresso.EspressoUtils.clickClickableSpan
 import com.orgzly.android.espresso.EspressoUtils.onListItem
+import com.orgzly.android.ui.MainActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 class InternalLinksTest : OrgzlyTest() {
     @get:Rule
@@ -40,6 +37,9 @@ class InternalLinksTest : OrgzlyTest() {
 
                     * Note [a-4]
                     file:book-b.org
+
+                    * Note [a-5]
+                    file:./book-b.org
                 """.trimIndent()
         )
 
@@ -64,11 +64,12 @@ class InternalLinksTest : OrgzlyTest() {
         )
 
         activityRule.launchActivity(null)
+
+        onListItem(0).perform(click())
     }
 
     @Test
     fun testDifferentCaseUuidInternalLink() {
-        onListItem(0).perform(click())
         onListItem(0).onChildView(withId(R.id.item_head_content))
                 .perform(clickClickableSpan("id:bdce923b-C3CD-41ED-B58E-8BDF8BABA54F"))
         onView(withId(R.id.fragment_note_title)).check(matches(withText("Note [b-2]")))
@@ -76,7 +77,6 @@ class InternalLinksTest : OrgzlyTest() {
 
     @Test
     fun testDifferentCaseCustomIdInternalLink() {
-        onListItem(0).perform(click())
         onListItem(1).onChildView(withId(R.id.item_head_content))
                 .perform(clickClickableSpan("#Different case custom id"))
         onView(withId(R.id.fragment_note_title)).check(matches(withText("Note [b-1]")))
@@ -84,7 +84,6 @@ class InternalLinksTest : OrgzlyTest() {
 
     @Test
     fun testCustomIdLink() {
-        onListItem(0).perform(click())
         onListItem(2).onChildView(withId(R.id.item_head_content))
                 .perform(clickClickableSpan("#Link to note in a different book"))
         onView(withId(R.id.fragment_note_title)).check(matches(withText("Note [b-3]")))
@@ -92,9 +91,16 @@ class InternalLinksTest : OrgzlyTest() {
 
     @Test
     fun testBookLink() {
-        onListItem(0).perform(click())
         onListItem(3).onChildView(withId(R.id.item_head_content))
                 .perform(clickClickableSpan("file:book-b.org"))
+        onView(withId(R.id.fragment_book_view_flipper)).check(matches(isDisplayed()))
+        onListItem(0).onChildView(withId(R.id.item_head_title)).check(matches(withText("Note [b-1]")))
+    }
+
+    @Test
+    fun testBookRelativeLink() {
+        onListItem(4).onChildView(withId(R.id.item_head_content))
+                .perform(clickClickableSpan("file:./book-b.org"))
         onView(withId(R.id.fragment_book_view_flipper)).check(matches(isDisplayed()))
         onListItem(0).onChildView(withId(R.id.item_head_title)).check(matches(withText("Note [b-1]")))
     }
