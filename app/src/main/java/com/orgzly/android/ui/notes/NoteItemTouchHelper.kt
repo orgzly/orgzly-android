a0899add9a7484f6182fa57233909662737521e2
Neven <neven@orgzly.com>
2019-02-23 15:04:20 +0100

    Add label to swipe action

--- app/src/main/java/com/orgzly/android/ui/notes/NoteItemTouchHelper.kt
+++ app/src/main/java/com/orgzly/android/ui/notes/NoteItemTouchHelper.kt
@@ -1,105 +1,73 @@
 package com.orgzly.android.ui.notes
 
-import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
-import android.graphics.drawable.ColorDrawable
 import android.view.View
-import androidx.annotation.ColorInt
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.orgzly.BuildConfig
-import com.orgzly.R
 import com.orgzly.android.util.LogUtils
 
-
 class NoteItemTouchHelper(val listener: Listener) : ItemTouchHelper(Callback(listener)) {
-//    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-//        super.onDraw(c, parent, state)
-//    }
 
     interface Listener {
         fun onSwipeLeft(id: Long)
     }
 
     class Callback(val listener: Listener) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.START) {
-        data class Attrs(@ColorInt val itemBg: Int, @ColorInt val labelBg: Int) {
-            companion object {
-                @SuppressWarnings("ResourceType")
-                fun getInstance(context: Context): Attrs {
-                    val typedArray = context.obtainStyledAttributes(intArrayOf(
-                            android.R.attr.windowBackground,
-                            R.attr.app_bar_bg_color))
 
-                    val attrs = Attrs(typedArray.getColor(0, 0), typedArray.getColor(1, 0))
-
-                    typedArray.recycle()
-
-                    return attrs
-                }
-            }
-        }
-
-        lateinit var attrs: Attrs
+        private var leftSwipeAction: SwipeAction? = null
 
         override fun isLongPressDragEnabled(): Boolean {
             return false
         }
 
-        override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+        override fun onChildDraw(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean) {
+
             val itemView = viewHolder.itemView
 
             val noteItemViewHolder = viewHolder as? NoteItemViewHolder ?: return
 
-            if (BuildConfig.LOG_DEBUG) LogUtils.d(TAG, itemView.left, itemView.right, dX, dY, actionState, isCurrentlyActive)
+            if (BuildConfig.LOG_DEBUG)
+                LogUtils.d(TAG, itemView.left, itemView.right, dX, dY, actionState, isCurrentlyActive)
 
-            if (dX < 0) { // Swiping left
+            when {
+                dX < 0 -> { // Swipe left
+                    // Hide indent
+                    noteItemViewHolder.indentContainer.visibility = View.INVISIBLE
 
-                // Hide indent
-                noteItemViewHolder.indentContainer.visibility = View.INVISIBLE
+                    if (leftSwipeAction == null) {
+                        leftSwipeAction = SwipeAction.OpenNote(recyclerView.context)
+                    }
 
-                if (!::attrs.isInitialized) {
-                    attrs = Attrs.getInstance(recyclerView.context)
+                    leftSwipeAction?.drawForLeftSwipe(canvas, itemView, dX)
                 }
 
-                // Label background
-                ColorDrawable(attrs.labelBg).apply {
-                    setBounds((itemView.right + dX).toInt(), itemView.top, itemView.right, itemView.bottom)
-                    draw(c)
+                dX > 0 -> { // Swipe right
                 }
 
-                // Item background
-                ColorDrawable(attrs.itemBg).apply {
-                    setBounds(itemView.left, itemView.top, (itemView.right + dX).toInt(), itemView.bottom)
-                    draw(c)
+                dX == 0f -> { // Original position
+                    noteItemViewHolder.indentContainer.visibility = View.VISIBLE
+
+                    // Reset so it can be re-initialized if settings changed
+                    leftSwipeAction = null
                 }
-
-                // Border
-
-                val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-                    color = attrs.labelBg
-                    style = Paint.Style.STROKE
-                }
-
-                val backgroundRectangle = RectF(
-                        itemView.left.toFloat() - 1,
-                        itemView.top.toFloat(),
-                        itemView.right.toFloat() + 1,
-                        itemView.bottom.toFloat())
-
-                c.drawRect(backgroundRectangle, backgroundPaint)
-
-
-            } else if (dX == 0f) { // Original position
-                noteItemViewHolder.indentContainer.visibility = View.VISIBLE
             }
 
-            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
         }
 
-        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder): Boolean {
+
             return true
         }
 
@@ -123,4 +91,5 @@ class NoteItemTouchHelper(val listener: Listener) : ItemTouchHelper(Callback(lis
     companion object {
         private val TAG = NoteItemTouchHelper::class.java.name
     }
+
 }
---
+++ app/src/main/java/com/orgzly/android/ui/notes/SwipeAction.kt
@@ -0,0 +1,92 @@
+package com.orgzly.android.ui.notes
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.Typeface
+import android.os.Build
+import android.view.View
+import androidx.annotation.StringRes
+import com.orgzly.R
+
+
+open class SwipeAction(val context: Context, @StringRes val res: Int) {
+    private val attrs = SwipeActionAttributes.getInstance(context)
+
+    private val labelText = context.getString(R.string.open).toUpperCase()
+
+    private val margin = context.resources.getDimension(R.dimen.screen_edge)
+
+    private val labelBgPaint: Paint
+
+    private val labelTextPaint: Paint
+
+    private val labelTextRect: Rect
+
+    private val borderPaint: Paint
+
+    private val itemBgPaint: Paint
+
+    init {
+        labelBgPaint = Paint().also { paint ->
+            paint.color = attrs.labelBg
+        }
+
+        labelTextPaint = Paint().also { paint ->
+            paint.color = attrs.labelColor
+            paint.textSize = attrs.labelSize.toFloat()
+            paint.textAlign = Paint.Align.CENTER
+            paint.style = Paint.Style.FILL
+            paint.isAntiAlias = true
+            paint.typeface = Typeface.DEFAULT_BOLD
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                paint.letterSpacing = 0.05f
+            }
+
+            labelTextRect = Rect().also { rect ->
+                paint.getTextBounds(labelText, 0, labelText.length, rect)
+            }
+        }
+
+        borderPaint = Paint().also { paint ->
+            paint.color = attrs.labelBg
+            paint.style = Paint.Style.STROKE
+        }
+
+        itemBgPaint = Paint().also { paint ->
+            paint.color = attrs.itemBg
+        }
+    }
+
+    fun drawForLeftSwipe(c: Canvas, itemView: View, dX: Float) {
+        c.drawRect(
+                itemView.right + dX,
+                itemView.top.toFloat(),
+                itemView.right.toFloat(),
+                itemView.bottom.toFloat(),
+                labelBgPaint)
+
+
+        val x = itemView.right - labelTextRect.width() / 2f - margin
+        val y = (itemView.top + itemView.bottom) / 2f - (labelTextPaint.descent() + labelTextPaint.ascent()) / 2
+        c.drawText(labelText, x, y, labelTextPaint)
+
+        c.drawRect(
+                itemView.left.toFloat(),
+                itemView.top.toFloat(),
+                itemView.right + dX,
+                itemView.bottom.toFloat(),
+                itemBgPaint)
+
+        c.drawRect(
+                itemView.left.toFloat() - 1,
+                itemView.top.toFloat(),
+                itemView.right.toFloat() + 1,
+                itemView.bottom.toFloat(),
+                borderPaint)
+    }
+
+    class OpenNote(context: Context) : SwipeAction(context, R.string.open)
+}
