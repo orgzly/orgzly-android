7756d599eb1bd88611265b66ab28982987931db3
Neven <neven@orgzly.com>
2019-05-02 20:29:50 +0200

    Check if path is absolute when displaying images
    Fixes #530.

--- app/src/main/java/com/orgzly/android/ui/ImageLoader.kt
+++ app/src/main/java/com/orgzly/android/ui/ImageLoader.kt
@@ -53,16 +53,21 @@ object ImageLoader {
             val context = App.getAppContext()
 
             // Get the file
-            val file = File(Environment.getExternalStorageDirectory(), path)
+            val file = if (path.startsWith('/')) {
+                File(path)
+            } else {
+                File(Environment.getExternalStorageDirectory(), path)
+            }
 
             if (file.exists()) {
                 // Get the Uri
-                val contentUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider", file)
+                val contentUri = FileProvider.getUriForFile(
+                        context, BuildConfig.APPLICATION_ID + ".fileprovider", file)
 
                 // Get image sizes to reduce their memory footprint by rescaling
                 val options = BitmapFactory.Options()
                 options.inJustDecodeBounds = true
-                BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().toString() + "/" + path, options)
+                BitmapFactory.decodeFile(file.absolutePath, options)
 
                 val size = calculateImageDisplaySize(
                         file.name, "pre-load", textWithMarkup, options.outWidth, options.outHeight)
@@ -111,6 +116,8 @@ object ImageLoader {
                                 text.setSpan(ImageSpan(bitmapDrawable), start, end, flags)
                             }
                         })
+            } else {
+                if (BuildConfig.LOG_DEBUG) LogUtils.d(TAG, "File $file (from $path) does not exist")
             }
         }
     }
