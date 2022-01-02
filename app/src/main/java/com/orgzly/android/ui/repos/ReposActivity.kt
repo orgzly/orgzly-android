63987af151650e1726d6079195f197efdeb349a5
Daniel Woffinden <daw@google.com>
2022-01-02 18:06:31 +0000

    Android R+: request MANAGE_EXTERNAL_STORAGE when creating new git repos

--- app/src/main/java/com/orgzly/android/ui/repos/ReposActivity.kt
+++ app/src/main/java/com/orgzly/android/ui/repos/ReposActivity.kt
@@ -1,8 +1,13 @@
 package com.orgzly.android.ui.repos
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
@@ -28,6 +33,7 @@ import com.orgzly.android.ui.repo.git.GitRepoActivity
 import com.orgzly.android.ui.repo.webdav.WebdavRepoActivity
 import com.orgzly.databinding.ActivityReposBinding
 import javax.inject.Inject
+
 
 /**
  * List of user-configured repositories.
@@ -220,11 +226,14 @@ class ReposActivity : CommonActivity(), AdapterView.OnItemClickListener, Activit
             }
 
             R.id.repos_options_menu_item_new_git -> {
-                if (ContextCompat.checkSelfPermission(this, READ_WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    GitRepoActivity.start(this)
-                } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+                    val uri = Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+                    startActivity(Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri))
+                } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R && ContextCompat.checkSelfPermission(this, READ_WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Show explanation why possibly, if ActivityCompat.shouldShowRequestPermissionRationale() says so?
                     ActivityCompat.requestPermissions(this, arrayOf(READ_WRITE_EXTERNAL_STORAGE), ACTIVITY_REQUEST_CODE_FOR_READ_WRITE_EXTERNAL_STORAGE)
+                } else {
+                    GitRepoActivity.start(this)
                 }
                 return
             }
