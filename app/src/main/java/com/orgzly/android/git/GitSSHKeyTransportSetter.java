56e232722bbad2bf30b6f3235ea0b8c995696a36
Victor Andreasson <victor.andreasson@fripost.org>
2022-07-18 18:33:38 +0200

    Git via SSH: Replace Jsch transport with Apache
    N.B. Requires API >= 26.
    
    Apache MINA SSHD is now the standard SSH transport library for JGit. It
    supports more modern key algorithms than Jsch.
    
    I have added a notification prompt to the user upon new or unexpected SSH
    server host key. I have no idea what I'm doing, but it seems to work.

--- app/src/main/java/com/orgzly/android/git/GitSSHKeyTransportSetter.java
+++ app/src/main/java/com/orgzly/android/git/GitSSHKeyTransportSetter.java
@@ -1,50 +1,74 @@
 package com.orgzly.android.git;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import android.net.Uri;
+import android.os.Build;
 
+import androidx.annotation.RequiresApi;
+
+import com.orgzly.android.App;
+
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.api.TransportCommand;
 import org.eclipse.jgit.api.TransportConfigCallback;
-import org.eclipse.jgit.transport.JschConfigSessionFactory;
-import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.internal.transport.sshd.OpenSshServerKeyDatabase;
 import org.eclipse.jgit.transport.SshSessionFactory;
 import org.eclipse.jgit.transport.SshTransport;
-import org.eclipse.jgit.transport.Transport;
-import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.transport.sshd.ServerKeyDatabase;
+import org.eclipse.jgit.transport.sshd.SshdSessionFactory;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 public class GitSSHKeyTransportSetter implements GitTransportSetter {
-    private String sshKeyPath;
-    private SshSessionFactory sshSessionFactory;
-    private TransportConfigCallback configCallback;
+    private final TransportConfigCallback configCallback;
 
     public GitSSHKeyTransportSetter(String pathToSSHKey) {
-        sshKeyPath = pathToSSHKey;
-        sshSessionFactory = new JschConfigSessionFactory() {
+
+        SshSessionFactory factory = new SshdSessionFactory(null, null) {
+
             @Override
-            protected void configure(OpenSshConfig.Host host, Session session ) {
-                session.setConfig("StrictHostKeyChecking", "no");
+            public File getHomeDirectory() {
+                return App.getAppContext().getFilesDir();
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            protected List<Path> getDefaultIdentities(File sshDir) {
+                return Collections.singletonList(Paths.get(Uri.decode(pathToSSHKey)));
             }
 
             @Override
-            protected JSch createDefaultJSch(FS fs) throws JSchException {
-                JSch defaultJSch = super.createDefaultJSch(fs);
-                defaultJSch.addIdentity(sshKeyPath);
-                return defaultJSch;
+            protected String getDefaultPreferredAuthentications() {
+                return "publickey";
             }
 
+            @Override
+            protected ServerKeyDatabase createServerKeyDatabase(@NonNull File homeDir,
+                                                                @NonNull File sshDir) {
+                // We override this method because we want to set "askAboutNewFile" to False.
+                return new OpenSshServerKeyDatabase(false,
+                        getDefaultKnownHostsFiles(sshDir));
+            }
         };
-        configCallback = new TransportConfigCallback() {
-            @Override
-            public void configure(Transport transport) {
-                SshTransport sshTransport = (SshTransport) transport;
-                sshTransport.setSshSessionFactory(sshSessionFactory);
-            }
+
+        SshSessionFactory.setInstance(factory);
+
+        // org.apache.sshd.common.config.keys.IdentityUtils freaks out if user.home is not set
+        System.setProperty("user.home", App.getAppContext().getFilesDir().toString());
+
+        configCallback = transport -> {
+            SshTransport sshTransport = (SshTransport) transport;
+            sshTransport.setSshSessionFactory(factory);
+
         };
     }
 
     public TransportCommand setTransport(TransportCommand tc) {
         tc.setTransportConfigCallback(configCallback);
+        tc.setCredentialsProvider(new SshCredentialsProvider());
         return tc;
     }
 }
